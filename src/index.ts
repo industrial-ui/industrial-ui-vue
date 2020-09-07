3514c66a987c689aca6da6e8c3da214b3668b4c5
VanishMax <maxkors2014@gmail.com>
2020-09-07 18:09:25 +0300

    Export all new components

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 export { default as Button } from './lib/components/Button.vue';
+
 export { default as Checkbox } from './lib/components/Checkbox.vue';
 export { default as CheckboxGroup } from './lib/components/CheckboxGroup.vue';
+
 export { default as Radio } from './lib/components/Radio.vue';
 export { default as RadioGroup } from './lib/components/RadioGroup.vue';
+
 export { default as Dropdown } from './lib/components/Dropdown.vue';
 export { default as DropdownSelect } from './lib/components/DropdownSelect.vue';
+
+export { default as Modal } from './lib/components/Modal.vue';
+
+export { default as Tabs } from './lib/components/Tabs.vue';
+export { default as TabPanel } from './lib/components/TabPanel.vue';
+
+export { default as Toggle } from './lib/components/Toggle.vue';
+
+export { default as CustomTransition } from './lib/components/Transition.vue';
 
 export { default as iui } from './lib/utils/plugin';
